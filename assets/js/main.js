d9b50c144a205ee6c0158a568c918f9f713a803f
Diarmuid Murphy <diarmuid.com@gmail.com>
2021-02-07 18:22:43 -0800

    refactor

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,17 @@ const winningScores = [
 	[4, 8],
 ];
 
+function drawMsg(status, cls='default') {
+	document.querySelector('#result').innerHTML = '';
+	let msg = document.createElement('div');
+	let heading = document.createElement('h1');
+	msg.setAttribute('class', `alert alert-${cls} p-4 m-4`);
+	heading.textContent = status;
+	msg.appendChild(heading);
+	
+	document.querySelector('#result').appendChild(msg);
+}
+
 function calcScore() {
 	let chiefsScore = document.querySelector('#chiefs-input').value;
 	let buccsScore = document.querySelector('#buccs-input').value;
@@ -19,42 +30,17 @@ function calcScore() {
 	chiefsScore = chiefsScore[chiefsScore.length - 1];
 	buccsScore = buccsScore[buccsScore.length - 1];
 
-	// console.log('chiefs:', chiefsScore);
-	// console.log('buccs:', buccsScore);
-
 	let toCompare = JSON.stringify([parseInt(chiefsScore), parseInt(buccsScore)]);
 
 	for (let i=0;i<winningScores.length;i++) {
-		document.querySelector('#result').innerHTML = '';
-		console.log(JSON.stringify(winningScores[i]));
-		console.log(toCompare);
-
 		if (JSON.stringify(winningScores[i]) === toCompare) {
-			let msg = document.createElement('div');
-			let heading = document.createElement('h1');
-			msg.setAttribute('class', 'alert alert-success p-4 m-4');
-			heading.textContent = 'Winning!';
-			msg.appendChild(heading);
-			
-			document.querySelector('#result').appendChild(msg);
+			drawMsg('Winning!', 'success');
 			break;
 		} else {
-			let msg = document.createElement('div');
-			let heading = document.createElement('h1');
-			msg.setAttribute('class', 'alert alert-danger p-4 m-4');
-			heading.textContent = 'Lame :(';
-			msg.appendChild(heading);
-			
-			document.querySelector('#result').appendChild(msg);
+			drawMsg('Lame :(', 'danger');
 		}
 	}
 }
 
-document.querySelector('#chiefs-input').addEventListener('input', () => {
-	calcScore();
-});
-
-document.querySelector('#buccs-input').addEventListener('input', () => {
-	calcScore();
-});
-
+document.querySelector('#chiefs-input').addEventListener('input', () => calcScore());
+document.querySelector('#buccs-input').addEventListener('input', () => calcScore());
